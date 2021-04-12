1bf1e7a36100cc664df26c204bcd1284229a219f
Luis García Montañés <gamo.luis@gmail.com>
2021-04-12 21:08:14 +0200

    Initial commit for Firebase integration

---
+++ what-is-on-your-mind/src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyAu3R_ZPSebhLYgZV3DghcSaA3vsBMq51o",
+    authDomain: "whatisonyourmind-9b11d.firebaseapp.com",
+    databaseURL:"https://whatisonyourmind-9b11d-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "whatisonyourmind-9b11d",
+    storageBucket: "whatisonyourmind-9b11d.appspot.com",
+    messagingSenderId: "589681057681",
+    appId: "1:589681057681:web:158f9b5fef34078acc8430"
+    };
+
+export default config
